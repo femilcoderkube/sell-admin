eec2aa53cbfb8ab06782fb0811aaa6cdf78bb262
Kishan <kishansutariya.coderkube@gmail.com>
2025-06-18 12:07:58 +0530

    changes

--- src/pages/AddLeague/index.tsx
+++ src/pages/AddLeague/index.tsx
@@ -177,14 +177,20 @@ const validationSchema = Yup.object().shape({
     .min(Yup.ref("startDate"), "End date must be after start date"),
 });
 
-
 const LeagueStep1: FC<StepProps> = ({ step }) => {
   const dispatch = useDispatch();
-  const { values, errors, touched, setFieldValue, handleChange } = useFormikContext();
+  const { values, errors, touched, setFieldValue, handleChange } =
+    useFormikContext();
 
-  const loadGameOptions = async (search: string, loadedOptions: any, { page }: any) => {
+  const loadGameOptions = async (
+    search: string,
+    loadedOptions: any,
+    { page }: any
+  ) => {
     const perPage = 10;
-    const response: any = await dispatch(fetchGames({ page, perPage, searchTerm: search }));
+    const response: any = await dispatch(
+      fetchGames({ page, perPage, searchTerm: search })
+    );
     const data = response.payload;
     const options: any[] = data?.data.result.map((game: any) => ({
       value: game._id,
@@ -198,9 +204,15 @@ const LeagueStep1: FC<StepProps> = ({ step }) => {
     };
   };
 
-  const loadDevicesOptions = async (search: string, loadedOptions: any, { page }: any) => {
+  const loadDevicesOptions = async (
+    search: string,
+    loadedOptions: any,
+    { page }: any
+  ) => {
     const perPage = 10;
-    const response: any = await dispatch(fetchDevices({ page, perPage, search }));
+    const response: any = await dispatch(
+      fetchDevices({ page, perPage, search })
+    );
     const data = response.payload;
     const options: any[] = data?.data.result.map((game: any) => ({
       value: game._id,
@@ -269,7 +281,8 @@ const LeagueStep1: FC<StepProps> = ({ step }) => {
             control: (base) => ({
               ...base,
               backgroundColor: "#212739",
-              border: touched.game && errors.game ? "1px solid #ef4444" : "none",
+              border:
+                touched.game && errors.game ? "1px solid #ef4444" : "none",
               borderRadius: "0.52rem",
               paddingLeft: "0.75rem",
               color: "#fff",
@@ -299,7 +312,11 @@ const LeagueStep1: FC<StepProps> = ({ step }) => {
             }),
             option: (base, { isFocused, isSelected }) => ({
               ...base,
-              backgroundColor: isSelected ? "#007EFF" : isFocused ? "#2B3245" : "#212739",
+              backgroundColor: isSelected
+                ? "#007EFF"
+                : isFocused
+                ? "#2B3245"
+                : "#212739",
               color: "#fff",
               fontSize: "0.78125rem",
               padding: "0.5rem 0.75rem",
@@ -312,7 +329,11 @@ const LeagueStep1: FC<StepProps> = ({ step }) => {
           }}
           components={{
             DropdownIndicator: () => (
-              <img src={downarr} alt="dropdown" style={{ width: "16px", marginRight: "10px" }} />
+              <img
+                src={downarr}
+                alt="dropdown"
+                style={{ width: "16px", marginRight: "10px" }}
+              />
             ),
           }}
         />
@@ -333,7 +354,8 @@ const LeagueStep1: FC<StepProps> = ({ step }) => {
             control: (base) => ({
               ...base,
               backgroundColor: "#212739",
-              border: touched.device && errors.device ? "1px solid #ef4444" : "none",
+              border:
+                touched.device && errors.device ? "1px solid #ef4444" : "none",
               borderRadius: "0.52rem",
               paddingLeft: "0.75rem",
               color: "#fff",
@@ -363,7 +385,11 @@ const LeagueStep1: FC<StepProps> = ({ step }) => {
             }),
             option: (base, { isFocused, isSelected }) => ({
               ...base,
-              backgroundColor: isSelected ? "#007EFF" : isFocused ? "#2B3245" : "#212739",
+              backgroundColor: isSelected
+                ? "#007EFF"
+                : isFocused
+                ? "#2B3245"
+                : "#212739",
               color: "#fff",
               fontSize: "0.78125rem",
               padding: "0.5rem 0.75rem",
@@ -376,7 +402,11 @@ const LeagueStep1: FC<StepProps> = ({ step }) => {
           }}
           components={{
             DropdownIndicator: () => (
-              <img src={downarr} alt="dropdown" style={{ width: "16px", marginRight: "10px" }} />
+              <img
+                src={downarr}
+                alt="dropdown"
+                style={{ width: "16px", marginRight: "10px" }}
+              />
             ),
           }}
         />
@@ -432,7 +462,9 @@ const LeagueStep1: FC<StepProps> = ({ step }) => {
           placeholder=" "
           disabled={values.format === "1v1"}
           className={`block w-full text-[0.78125rem] text-white focus:outline-0 focus:!border focus:!border-[#2792FF] pt-[1.5rem] pb-[0.35rem] bg-input-color rounded-[0.52rem] px-3 block appearance-none leading-normal ${
-            touched.playersPerTeam && errors.playersPerTeam ? "border border-red-500" : ""
+            touched.playersPerTeam && errors.playersPerTeam
+              ? "border border-red-500"
+              : ""
           } ${values.format === "1v1" ? "opacity-50 cursor-not-allowed" : ""}`}
         />
         <label
@@ -442,17 +474,23 @@ const LeagueStep1: FC<StepProps> = ({ step }) => {
           Players Per Team
         </label>
         {touched.playersPerTeam && errors.playersPerTeam && (
-          <div className="text-red-500 text-[0.7rem] mt-1">{errors.playersPerTeam}</div>
+          <div className="text-red-500 text-[0.7rem] mt-1">
+            {errors.playersPerTeam}
+          </div>
         )}
       </div>
       <div className="grid grid-cols-2 gap-3 mb-4">
         <div className="relative float-label-input custom-input">
           <DatePicker
             selected={values.startDate ? new Date(values.startDate) : null}
-            onChange={(date: Date) => setFieldValue("startDate", date.toISOString().split("T")[0])}
+            onChange={(date: Date) =>
+              setFieldValue("startDate", date.toISOString().split("T")[0])
+            }
             dateFormat="yyyy-MM-dd"
             className={`block w-full text-[0.78125rem] text-white focus:outline-0 focus:!border focus:!border-[#2792FF] pt-[1.5rem] pb-[0.35rem] bg-input-color rounded-[0.52rem] px-3 block appearance-none leading-normal ${
-              touched.startDate && errors.startDate ? "border border-red-500" : ""
+              touched.startDate && errors.startDate
+                ? "border border-red-500"
+                : ""
             }`}
             id="startDate"
             name="startDate"
@@ -470,13 +508,17 @@ const LeagueStep1: FC<StepProps> = ({ step }) => {
             Start Date
           </label>
           {touched.startDate && errors.startDate && (
-            <div className="text-red-500 text-[0.7rem] mt-1">{errors.startDate}</div>
+            <div className="text-red-500 text-[0.7rem] mt-1">
+              {errors.startDate}
+            </div>
           )}
         </div>
         <div className="relative float-label-input custom-input">
           <DatePicker
             selected={values.endDate ? new Date(values.endDate) : null}
-            onChange={(date: Date) => setFieldValue("endDate", date.toISOString().split("T")[0])}
+            onChange={(date: Date) =>
+              setFieldValue("endDate", date.toISOString().split("T")[0])
+            }
             dateFormat="yyyy-MM-dd"
             className={`block w-full text-[0.78125rem] text-white focus:outline-0 focus:!border focus:!border-[#2792FF] pt-[1.5rem] pb-[0.35rem] bg-input-color rounded-[0.52rem] px-3 block appearance-none leading-normal ${
               touched.endDate && errors.endDate ? "border border-red-500" : ""
@@ -497,7 +539,9 @@ const LeagueStep1: FC<StepProps> = ({ step }) => {
             End Date
           </label>
           {touched.endDate && errors.endDate && (
-            <div className="text-red-500 text-[0.7rem] mt-1">{errors.endDate}</div>
+            <div className="text-red-500 text-[0.7rem] mt-1">
+              {errors.endDate}
+            </div>
           )}
         </div>
       </div>
@@ -513,7 +557,7 @@ const LeagueStep1: FC<StepProps> = ({ step }) => {
           font-family: inherit;
         }
         .custom-datepicker .react-datepicker__header {
-          background-color: #2B3245;
+          background-color: #2b3245;
           border-bottom: none;
           padding: 0.5rem;
         }
@@ -528,15 +572,15 @@ const LeagueStep1: FC<StepProps> = ({ step }) => {
           padding: 0.3rem;
         }
         .custom-datepicker .react-datepicker__day:hover {
-          background-color: #2B3245;
+          background-color: #2b3245;
         }
         .custom-datepicker .react-datepicker__day--selected,
         .custom-datepicker .react-datepicker__day--keyboard-selected {
-          background-color: #007EFF;
+          background-color: #007eff;
           color: #fff;
         }
         .custom-datepicker .react-datepicker__day--disabled {
-          color: #6B7280;
+          color: #6b7280;
           opacity: 0.5;
         }
         .custom-datepicker .react-datepicker__navigation {
@@ -552,9 +596,6 @@ const LeagueStep1: FC<StepProps> = ({ step }) => {
     </div>
   );
 };
-
-
-
 
 // // Step 1: General Information
 // const LeagueStep1: FC<StepProps> = ({ step }) => {
@@ -1022,11 +1063,10 @@ const LeagueStep2: FC<StepProps> = ({ step }) => {
                 {errors.queueSettings.schedule.days}
               </div>
             )}
-                <div className="grid grid-cols-2 gap-3 mt-4">
+          <div className="grid grid-cols-2 gap-3 mt-4">
             <TimePickerField
               label="Start Time"
               name="queueSettings.schedule.startTime"
-
             />
             <TimePickerField
               label="End Time"
@@ -1226,7 +1266,7 @@ const LeagueStep2: FC<StepProps> = ({ step }) => {
           )}
         </FieldArray>
       </div>
-
+      {/* 
       <div className="mb-4">
         <h5 className="text-white mb-2 text-base font-medium">
           Custom Registration Fields
@@ -1272,7 +1312,7 @@ const LeagueStep2: FC<StepProps> = ({ step }) => {
                       htmlFor={`customRegistrationFields[${index}].fieldName`}
                       className="absolute top-3 left-0 translate-y-[0.2rem] font-bold text-[0.78125rem] pointer-events-none transition duration-200 bg-transparent px-3 text-custom-gray"
                     >
-                      Field Name
+                      Game ID
                     </label>
                     {touched.customRegistrationFields?.[index]?.fieldName &&
                       errors.customRegistrationFields?.[index]?.fieldName && (
@@ -1323,6 +1363,125 @@ const LeagueStep2: FC<StepProps> = ({ step }) => {
                         type="checkbox"
                         name={`customRegistrationFields[${index}].required`}
                         className="sr-only peer"
+                      />
+                      <div className="relative w-9 h-5 bg-custom-gray focus:outline-none rounded-full peer dark:bg-custom-gray peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all dark:border-gray-500 peer-checked:bg-primary-gradient dark:peer-checked:bg-primary-gradient"></div>
+                    </label>
+                  </div>
+                </div>
+              ))}
+              <button
+                type="button"
+                onClick={() =>
+                  push({ fieldName: "", fieldType: "text", required: false })
+                }
+                className="w-full py-[0.45rem] border bg-input-color bg-opacity-40 rounded-[0.52rem] border-dashed border-custom-gray border-opacity-40 text-custom-gray text-[0.78125rem] font-medium"
+              >
+                + Add Custom Field
+              </button>
+            </>
+          )}
+        </FieldArray>
+      </div> */}
+      <div className="mb-4">
+        <h5 className="text-white mb-2 text-base font-medium">
+          Custom Registration Fields
+        </h5>
+        <FieldArray name="customRegistrationFields">
+          {({ push, remove }) => (
+            <>
+              {values.customRegistrationFields.map((field, index) => (
+                <div
+                  key={index}
+                  className="mb-4 bg-input-color p-4 rounded-[0.52rem]"
+                >
+                  <div className="flex justify-between items-center mb-2">
+                    <h6 className="text-white text-[0.78125rem] font-medium">
+                      Field #{index + 1}
+                    </h6>
+                    {index !== 0 && ( // Only show delete button for fields after the first one
+                      <button
+                        type="button"
+                        onClick={() => remove(index)}
+                        className="bg-gray-gradient hover:opacity-80 p-[0.625rem] rounded-[0.52rem] duration-300"
+                      >
+                        <img
+                          src={deleteIcon}
+                          alt="Delete"
+                          style={{ width: "1.25rem" }}
+                        />
+                      </button>
+                    )}
+                  </div>
+                  <div className="relative float-label-input custom-input mb-4">
+                    <Field
+                      type="text"
+                      id={`customRegistrationFields[${index}].fieldName`}
+                      name={`customRegistrationFields[${index}].fieldName`}
+                      placeholder=" "
+                      className={`block w-full text-[0.78125rem] text-white focus:outline-0 focus:!border focus:!border-[#2792FF] pt-[1.5rem] pb-[0.35rem] bg-[#2B3245] rounded-[0.52rem] px-3 block appearance-none leading-normal ${
+                        touched.customRegistrationFields?.[index]?.fieldName &&
+                        errors.customRegistrationFields?.[index]?.fieldName
+                          ? "border border-red-500"
+                          : ""
+                      }`}
+                    />
+                    <label
+                      htmlFor={`customRegistrationFields[${index}].fieldName`}
+                      className="absolute top-3 left-0 translate-y-[0.2rem] font-bold text-[0.78125rem] pointer-events-none transition duration-200 bg-transparent px-3 text-custom-gray"
+                    >
+                      Game ID
+                    </label>
+                    {touched.customRegistrationFields?.[index]?.fieldName &&
+                      errors.customRegistrationFields?.[index]?.fieldName && (
+                        <div className="text-red-500 text-[0.7rem] mt-1">
+                          {errors.customRegistrationFields[index].fieldName}
+                        </div>
+                      )}
+                  </div>
+                  <div className="relative flex-1 custom-input mb-4">
+                    <label
+                      htmlFor={`customRegistrationFields[${index}].fieldType`}
+                      className="absolute top-3 left-0 translate-y-[-0.3rem] font-bold text-[0.78125rem] pointer-events-none transition duration-200 bg-transparent px-3 text-custom-gray"
+                    >
+                      Field Type
+                    </label>
+                    <Field
+                      as="select"
+                      id={`customRegistrationFields[${index}].fieldType`}
+                      name={`customRegistrationFields[${index}].fieldType`}
+                      className={`block w-full text-[0.78125rem] text-white focus:outline-0 focus:!border focus:!border-[#2792FF] pt-[1.5rem] pb-[0.35rem] bg-[#2B3245] rounded-[0.52rem] px-3 block appearance-none leading-normal ${
+                        touched.customRegistrationFields?.[index]?.fieldType &&
+                        errors.customRegistrationFields?.[index]?.fieldType
+                          ? "border border-red-500"
+                          : ""
+                      }`}
+                      style={{
+                        backgroundImage: `url(${downarr})`,
+                        backgroundRepeat: "no-repeat",
+                        backgroundPosition: "right 10px center",
+                        backgroundSize: "16px 16px",
+                      }}
+                    >
+                      <option value="text">Text</option>
+                      <option value="number">Number</option>
+                      <option value="select">Select</option>
+                    </Field>
+                    {touched.customRegistrationFields?.[index]?.fieldType &&
+                      errors.customRegistrationFields?.[index]?.fieldType && (
+                        <div className="text-red-500 text-[0.7rem] mt-1">
+                          {errors.customRegistrationFields[index].fieldType}
+                        </div>
+                      )}
+                  </div>
+                  <div className="check_setting flex items-center justify-between w-full text-[0.78125rem] text-custom-gray focus:outline-0 focus:!border focus:!border-[#2792FF] py-[0.92rem] bg-[#2B3245] rounded-[0.52rem] px-3 block appearance-none leading-normal">
+                    <span className="text-white font-medium">Required</span>
+                    <label className="inline-flex items-center cursor-pointer">
+                      <Field
+                        type="checkbox"
+                        name={`customRegistrationFields[${index}].required`}
+                        className="sr-only peer"
+                        checked={index === 0 ? true : field.required} // First field is always required
+                        disabled={index === 0} // Disable checkbox for first field
                       />
                       <div className="relative w-9 h-5 bg-custom-gray focus:outline-none rounded-full peer dark:bg-custom-gray peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all dark:border-gray-500 peer-checked:bg-primary-gradient dark:peer-checked:bg-primary-gradient"></div>
                     </label>
@@ -1450,7 +1609,7 @@ export const AddLeague: FC = () => {
   const dispatch = useDispatch();
   const [step, setStep] = useState(1);
   const [showModal, setShowModal] = useState(false);
-  let pID = window.location.pathname.split("/")[1];;
+  let pID = window.location.pathname.split("/")[1];
 
   // Initialize form values
   const [initialValues, setInitialValues] = useState<League>({
