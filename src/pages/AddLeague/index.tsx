38fc99541566183b0a727b41597d6a0a19dc7704
Kishan <kishansutariya.coderkube@gmail.com>
2025-06-30 09:05:01 +0530

    changes

--- src/pages/AddLeague/index.tsx
+++ src/pages/AddLeague/index.tsx
@@ -1137,7 +1137,7 @@ const LeagueStep2: FC<StepProps> = ({ step }) => {
                     <Field
                       as="input"
                       type="text"
-                      readOnly={index == 0}
+                      readOnly={index === 0}
                       id={`customRegistrationFields[${index}].fieldName`}
                       name={`customRegistrationFields[${index}].fieldName`}
                       className={`block w-full text-[0.78125rem] text-white focus:outline-0 focus:!border focus:!border-[#2792FF] pt-[1.5rem] pb-[0.35rem] bg-[#2B3245] rounded-[0.52rem] px-3 leading-normal ${
@@ -1208,7 +1208,7 @@ const LeagueStep2: FC<StepProps> = ({ step }) => {
                 type="button"
                 onClick={() =>
                   push({
-                    fieldName: "Game ID",
+                    fieldName: "",
                     fieldType: "text",
                     required: false,
                   })
