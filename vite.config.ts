d9edb697c92e61eb9b4ff90593cd4e26b0909b61
Kishan <kishansutariya.coderkube@gmail.com>
2025-08-13 15:07:44 +0530

    changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,47 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
+import { VitePWA } from "vite-plugin-pwa";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Sell Admin",
+        short_name: "Sell Admin",
+        scope: "/",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#ffffff",
+        theme_color: "#000000",
+        icons: [
+          {
+            src: "/maskable_icon_x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "favicon",
+          },
+          {
+            src: "/maskable_icon_x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "favicon",
+          },
+          {
+            src: "/maskable_icon.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+        maximumFileSizeToCacheInBytes: 3000000,
+      },
+    }),
+  ],
   optimizeDeps: {
     exclude: ["lucide-react"],
   },
