cd4b48c3d3602dab0aa3f2f3209bf1d8e00b1a95
Kishan <kishansutariya.coderkube@gmail.com>
2025-08-18 12:09:32 +0530

    changes

--- src/pages/League/SeedDetail.tsx
+++ src/pages/League/SeedDetail.tsx
@@ -890,13 +890,13 @@ export const SeedDetail: FC<{ title: string }> = ({ title }) => {
             <label className="relative inline-flex items-center cursor-pointer mt-4">
               <input
                 type="checkbox"
-                checked={data?.isDeactivate}
+                checked={!data?.isDeactivate}
                 onChange={handleToggleActive}
                 className="sr-only peer"
               />
               <div className="w-11 h-6 bg-gray-700 peer-focus:outline-none peer-focus:ring-2 peer-focus:ring-blue-500 rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-blue-600"></div>
               <span className="ml-3 text-sm font-medium text-gray-300">
-                {isDraftActive ? "Active" : "Inactive"}
+                {!data?.isDeactivate ? "Active" : "Inactive"}
               </span>
             </label>
           </div>
