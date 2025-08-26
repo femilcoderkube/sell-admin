64a5cc4d159218c16daac76bdd7d11ff08367208
Kishan <kishansutariya.coderkube@gmail.com>
2025-08-26 17:25:06 +0530

    changes

--- src/components/NotificationPopup/PopupsTable.tsx
+++ src/components/NotificationPopup/PopupsTable.tsx
@@ -133,8 +133,9 @@ export const PopupsTable: React.FC<IPopupsProps> = ({
                   />
                   <div className="w-11 h-6 bg-gray-600 peer-focus:outline-none peer-focus:ring-2 peer-focus:ring-blue-400 rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-green-500"></div>
                   <span className="ml-2 text-xs font-medium text-gray-300">
-                    {tdetail.status.charAt(0).toUpperCase() +
-                      tdetail.status.slice(1)}
+                    {/* {tdetail.status.charAt(0).toUpperCase() +
+                      tdetail.status.slice(1)} */}
+                    Publish
                   </span>
                 </label>
               </td>
