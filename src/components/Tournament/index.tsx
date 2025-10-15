f7f1e29daefb89f5124f3a9fd56098ccce1f26c7
Kishan <femil.coderkube@gmail.com>
2025-10-15 12:05:41 +0530

    changes

--- src/components/Tournament/index.tsx
+++ src/components/Tournament/index.tsx
@@ -186,9 +186,9 @@ export const Tournamentes: React.FC = ({ title }: any) => {
                 value={perPage}
                 onChange={handlePerPageChange}
               >
-                <option value={10}>10</option>
-                <option value={25}>25</option>
-                <option value={50}>50</option>
+                <option value={12}>12</option>
+                <option value={24}>24</option>
+                <option value={36}>36</option>
               </select>
             </div>
             <form
