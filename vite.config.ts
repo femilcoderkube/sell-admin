d8b3f7bee3ceefd982ba4859e3fbf845951cf3bc
ashraf-ck <134932420+ashrafcoderkube@users.noreply.github.com>
2025-10-21 16:00:00 +0530

    sdf

--- vite.config.ts
+++ vite.config.ts
@@ -40,7 +40,7 @@ export default defineConfig({
       },
       workbox: {
         globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
-        maximumFileSizeToCacheInBytes: 3000000,
+        maximumFileSizeToCacheInBytes: 5000000,
       },
     }),
     javascriptObfuscator({
